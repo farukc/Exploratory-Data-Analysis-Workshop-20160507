51af5ec55f1a8e66508d7a21b708d94fb32ee809
İmran Kocabıyık <imrankocabiyik@gmail.com>
2016-03-23 16:18:07 +0200

    Adding various layouts and d3.js library

--- hiearchicalClustering.R
+++ hiearchicalClustering.R
@@ -62,4 +62,138 @@ class(cityDf$boylam) <- "numeric"
 dim(cityDf)
 cityDist <- dist(cityDf)
 hc <- hclust(cityDist)
-plot(hc)
+plot(hc, cex = 0.7)
+
+# change the layout
+# install.packages("ape")
+library(ape)
+
+# horizontal
+plot(as.phylo(hc), cex = 0.7, label.offset = 0.1)
+
+# fan
+plot(as.phylo(hc), type = "fan")
+
+
+# d3.js
+library(rjson)
+
+#convert output from hclust into a nested JSON file
+
+HCtoJSON<-function(hc){
+        
+        labels<-hc$labels
+        merge<-data.frame(hc$merge)
+        
+        for (i in (1:nrow(merge))) {
+                
+                if (merge[i,1]<0 & merge[i,2]<0) {eval(parse(text=paste0("node", i, "<-list(name=\"node", i, "\", children=list(list(name=labels[-merge[i,1]]),list(name=labels[-merge[i,2]])))")))}
+                else if (merge[i,1]>0 & merge[i,2]<0) {eval(parse(text=paste0("node", i, "<-list(name=\"node", i, "\", children=list(node", merge[i,1], ", list(name=labels[-merge[i,2]])))")))}
+                else if (merge[i,1]<0 & merge[i,2]>0) {eval(parse(text=paste0("node", i, "<-list(name=\"node", i, "\", children=list(list(name=labels[-merge[i,1]]), node", merge[i,2],"))")))}
+                else if (merge[i,1]>0 & merge[i,2]>0) {eval(parse(text=paste0("node", i, "<-list(name=\"node", i, "\", children=list(node",merge[i,1] , ", node" , merge[i,2]," ))")))}
+        }
+        
+        eval(parse(text=paste0("JSON<-toJSON(node",nrow(merge), ")")))
+        
+        return(JSON)
+}
+
+
+# wrap nested JSON file into d3 html
+
+D3Dendo<-function(JSON, text=8, height=800, width=700, file_out){
+        
+        header<-paste0("<!DOCTYPE html>
+                 <meta charset=\"utf-8\">
+                 <style>
+                 
+                 .node circle {
+                 fill: #fff;
+                 stroke: steelblue;
+                 stroke-width: 1.5px;
+                 }
+                 
+                 .node {
+                 font: ",text , "px sans-serif;
+                 }
+                 
+                 .link {
+                 fill: none;
+                 stroke: #ccc;
+                 stroke-width: 1.5px;
+                 }
+                 
+                 </style>
+                 <body>
+                 <script src=\"http://d3js.org/d3.v3.min.js\"></script>
+                 
+                 <script type=\"application/json\" id=\"data\">")
+        
+        
+        footer<-paste0("</script>
+  
+  
+  
+  
+  <script>
+  
+  var data = document.getElementById('data').innerHTML;
+  root = JSON.parse(data);
+  
+  
+  var width = ", width, ",
+  height = ", height, ";
+  
+  var cluster = d3.layout.cluster()
+  .size([height-50, width - 160]);
+  
+  var diagonal = d3.svg.diagonal()
+  .projection(function(d) { return [d.y, d.x]; });
+  
+  var svg = d3.select(\"body\").append(\"svg\")
+  .attr(\"width\", width)
+  .attr(\"height\", height)
+  .append(\"g\")
+  .attr(\"transform\", \"translate(40,0)\");
+  
+  
+  var nodes = cluster.nodes(root),
+  links = cluster.links(nodes);
+  
+  var link = svg.selectAll(\".link\")
+  .data(links)
+  .enter().append(\"path\")
+  .attr(\"class\", \"link\")
+  .attr(\"d\", diagonal);
+  
+  var node = svg.selectAll(\".node\")
+  .data(nodes)
+  .enter().append(\"g\")
+  .attr(\"class\", \"node\")
+  .attr(\"transform\", function(d) { return \"translate(\" + d.y + \",\" + d.x + \")\"; })
+  
+  node.append(\"circle\")
+  .attr(\"r\", 4.5);
+  
+  node.append(\"text\")
+  .attr(\"dx\", function(d) { return d.children ? 8 : 8; })
+  .attr(\"dy\", function(d) { return d.children ? 20 : 4; })
+  .style(\"text-anchor\", function(d) { return d.children ? \"end\" : \"start\"; })
+  .text(function(d) { return d.name; });
+  
+  
+  d3.select(self.frameElement).style(\"height\", height + \"px\");
+  
+  </script>") 
+        
+        cityDf<-file(file_out)
+        writeLines(paste0(header, JSON, footer), cityDf)
+        close(cityDf)
+        
+}
+
+
+
+hc <- hclust(dist(cityDf), "ave")
+JSON<-HCtoJSON(hc)
+D3Dendo(JSON, file_out="output.html")
