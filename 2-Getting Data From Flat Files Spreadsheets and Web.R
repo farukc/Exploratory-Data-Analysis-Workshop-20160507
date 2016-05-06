d8681d0562a1afe2053b59bb53764ed11bbe2ffe
İmran Kocabıyık <imrankocabiyik@gmail.com>
2016-05-06 22:25:21 +0300

    Typo

--- 2-Getting Data From Flat Files Spreadsheets and Web.R
+++ 2-Getting Data From Flat Files Spreadsheets and Web.R
@@ -3,11 +3,11 @@
 # 1. Getting data from flat files -----
 
 # file url to be downloaded
-url <- "https://stats.oecd.org/sdmx-json/data/DP_LIVE/.PISAMATH.GIRL+BOY../OECD?contentType=csv&detail=code&separator=comma&csv-lang=en"
+pisaUrl <- "https://stats.oecd.org/sdmx-json/data/DP_LIVE/.PISAMATH.GIRL+BOY../OECD?contentType=csv&detail=code&separator=comma&csv-lang=en"
 
 # download file
 if(!file.exists("pisa.csv")) {
-        download.file(url, destfile = "pisa.csv")
+        download.file(pisaUrl, destfile = "pisa.csv")
 }
 
 # load file
@@ -17,22 +17,16 @@ pisaData <- read.csv("pisa.csv", stringsAsFactors = FALSE)
 
 # getting the data from TUIK (Turkish Statistics Institute)
 maleURL <- "http://tuik.gov.tr/PreIstatistikTablo.do?istab_id=1332"
-femaleURL <- "http://tuik.gov.tr/PreIstatistikTablo.do?istab_id=1331"
 
 # download files
 if(!file.exists("male.xls")){
         download.file(maleURL, destfile = "male.xls") 
 }
 
-if(!file.exists("female.xls")){
-        download.file(femaleURL, destfile = "female.xls") 
-}
-
 # load data into R
 # install.packages("readxl")
 library(readxl)
 maleNames <- read_excel("male.xls", sheet=1)
-femaleNames <- read_excel("female.xls", sheet = 1)
 
 # Example 3: Getting Data from Web -----
 library(XML)
